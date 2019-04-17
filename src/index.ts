cf96bca4ca83af6c0e57310834a0b9881b84e2cd
david <davidqhr@gmail.com>
2019-04-17 11:26:11 +0800

    remove formatProps

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import { Wallet, formatProps } from "./components";
+import { Wallet } from "./components";
 import { connector, Connector } from "./connector";
 import { WalletReducer } from "./reducers";
 import { get64BytesString } from "./wallets";
 import "../index.css";
 
-export {
-  Wallet,
-  connector,
-  Connector,
-  WalletReducer,
-  formatProps,
-  get64BytesString
-};
+export { Wallet, connector, Connector, WalletReducer, get64BytesString };
