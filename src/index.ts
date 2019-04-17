8885e57e243ed5b6df419654f222eb34233e362e
GongHe <gonghe@ddex.io>
2019-04-17 21:19:17 +0800

    fix dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,4 @@ import { Wallet, WalletButton } from "./components";
 import { WalletReducer } from "./reducers";
 import "../index.css";
 
-declare global {
-  interface Window {
-    web3: any;
-    ethereum: any;
-  }
-}
-
 export { Wallet, WalletReducer, WalletButton };
