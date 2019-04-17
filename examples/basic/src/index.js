063bf6372870a693cb2bdb60e77a1c010afee896
david <davidqhr@gmail.com>
2019-04-17 11:26:38 +0800

    remove useless comments

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -4,20 +4,11 @@ import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
 import { Wallet, WalletReducer } from "hydro-sdk-wallet";
 
-// import { reducer as reduxFormReducer } from "redux-form";
-// import {
-//   App,
-//   Code,
-//   Markdown,
-//   Values,
-//   generateExampleBreadcrumbs
-// } from "redux-form-website-template";
-
 const dest = document.getElementById("content");
 const reducer = combineReducers({
   WalletReducer
-  // form: reduxFormReducer // mounted under "form"
 });
+
 const store = (window.devToolsExtension
   ? window.devToolsExtension()(createStore)
   : createStore)(reducer);
